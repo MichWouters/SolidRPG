a46f853b4766fb3e898e149dc9c3776fc41956ea
Michiel Wouters <michiel_woute64@hotmail.com>
2024-05-03 11:00:39 +0200

    Added sample Moq Unit Test

---
+++ RPG13.Business.Tests/Player/PlayerTests.cs
@@ -0,0 +1,58 @@
+﻿using Moq;
+using RPG13.Business.Enemies;
+using RPG13.Business.Factories.Interfaces;
+using RPG13.Business.Logging;
+using RPG13.Business.Player;
+using RPG13.Business.Service;
+using RPG13.Business.Services;
+
+namespace RPG13.Business.Tests.Player
+{
+    internal class PlayerTests
+    {
+        [Test]
+        public void WhenPlayerAttacks_ThenDamageDone_ShouldBeAddedToTotalDamage()
+        {
+            // Arrange -> Gather all data necessary here
+            Mock<ILogger> mockLogger = new Mock<ILogger>();
+            Mock<IDiceService> mockDiceService = new Mock<IDiceService>();
+            Mock<IRandomService> mockRandomService = new Mock<IRandomService>();
+            Mock<IWeaponsFactory> mockWeaponsFactory = new Mock<IWeaponsFactory>();
+
+            string name = "Foobar the feeble";
+            int expectedDamage = 0;
+
+            IPlayer playerUnderTest = new Human(mockDiceService.Object, mockLogger.Object, name);
+            IEnemy unluckyGoblin = new Goblin(mockRandomService.Object, mockWeaponsFactory.Object, mockLogger.Object);
+
+            // Act -> Execute the method under test here
+            playerUnderTest.Attack(unluckyGoblin);
+
+            // Assert -> Validate that the result is what was expected
+            Assert.That(playerUnderTest.DamageDone, Is.EqualTo(expectedDamage));
+        }
+
+
+        [Test]
+        public void WhenPlayerTakesDamage_TheirHealthIsDecreased()
+        {
+            // Arrange
+            int playerHP = 100;
+            int expectedHP = 95;
+            int damage = 5;
+
+            Mock<IDiceService> mockDiceService = new Mock<IDiceService>();
+            Mock<ILogger> mockLogger = new Mock<ILogger>();
+            string name = "Foobar the feeble";
+
+            IPlayer unluckyPlayer = new Human(mockDiceService.Object, mockLogger.Object, name);
+            unluckyPlayer.Health = playerHP;
+
+            // Act
+            unluckyPlayer.TakeDamage(damage);
+
+            // Assert
+            Assert.That(unluckyPlayer.Health, Is.EqualTo(expectedHP));
+        }
+    }
+}
